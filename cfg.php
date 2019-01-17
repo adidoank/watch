828be5c6c56833c48c45f746b1625858a9932045
adidoank <42825332+adidoank@users.noreply.github.com>
2019-01-18 03:27:00 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,15 @@
+<?php
+/* Seting opsiaonal */
+$delay=30;
+
+//multiple account
+$config = [
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh5DQrrp-oMDOe86xio-lf3my24Omhpu0q5SbhLDOzYqKj9y7rZGws7h6la2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=9424a08acec117a4",
+
+];
+
+
+
+
+
+?>
